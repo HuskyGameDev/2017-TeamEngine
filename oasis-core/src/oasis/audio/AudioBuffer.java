e84619e5b96657d10e3fc70c2cef5b538a02ed71
Nicholas Hamilton <nrhamilt@mtu.edu>
2017-12-07 18:27:11 -0500

    Begin documenting audio

--- oasis-core/src/oasis/audio/AudioBuffer.java
+++ oasis-core/src/oasis/audio/AudioBuffer.java
@@ -1,12 +1,41 @@
 package oasis.audio;
 
+/**
+ * 
+ * Holds PCM audio data 
+ * 
+ * Do NOT implement this, only use AudioBuffers created from 
+ * AudioDevice
+ * 
+ * @author Nicholas Hamilton 
+ *
+ */
 public interface AudioBuffer {
 
+    /**
+     * Dispose resource
+     */
     void dispose(); 
     
+    /**
+     * @return Audio format used by buffer 
+     */
     AudioFormat getFormat(); 
     
+    /**
+     * Set buffer data 
+     * 
+     * @param data data in bytes
+     * @param freq frequency
+     */
     void setData(byte[] data, int freq); 
+    
+    /**
+     * Set buffer data 
+     * 
+     * @param data data in shorts
+     * @param freq frequency
+     */
     void setData(short[] data, int freq); 
     
 }
--- oasis-core/src/oasis/audio/AudioDevice.java
+++ oasis-core/src/oasis/audio/AudioDevice.java
@@ -1,26 +1,82 @@
 package oasis.audio;
 
+/**
+ * 
+ * Manages audio resources 
+ * 
+ * @author Nicholas Hamilton 
+ *
+ */
 public interface AudioDevice {
 
     // resource creation
     
+    /**
+     * @return AudioListener 
+     */
     AudioListener createListener(); 
+    
+    /**
+     * @return AudioSource 
+     */
     AudioSource createSource(); 
+    
+    /**
+     * @param fmt audio format 
+     * @return AudioBuffer with corresponding format 
+     */
     AudioBuffer createBuffer(AudioFormat fmt); 
+    
+    /**
+     * Loads an AudioBuffer with WAV data
+     * 
+     * @param file file name 
+     * @return AudioBuffer with WAV data already set 
+     */
     AudioBuffer loadWAVBuffer(String file); 
     
     // getters
     
+    /**
+     * @return the current listener 
+     */
     AudioListener getListener(); 
+    
+    /**
+     * @return audio source at index 
+     */
     AudioSource getSource(int index); 
+    
+    /**
+     * @return current source count 
+     */
     int getSourceCount(); 
+    
+    /**
+     * @return maximum number of current sources 
+     */
     int getMaxSourceCount(); 
     
     // setters
     
+    /**
+     * set current listener 
+     */
     void setListener(AudioListener listener); 
+    
+    /**
+     * make a source current 
+     */
     void addSource(AudioSource source);
+    
+    /**
+     * make a source no longer current 
+     */
     void removeSource(AudioSource source); 
+    
+    /** 
+     * remove all sources form being current 
+     */
     void clearSources(); 
     
 }
